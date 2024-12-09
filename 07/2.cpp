2fe7773d0f8c653459437dc863a5599669cc6df4
Alexander Chinyakov <a.chinyakov@drweb.com>
2024-12-09 13:32:09 +0300

    day 7 part 2 solution

---
+++ 07/2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+int main( int argc, char * argv[] )
+{
+	if( argc < 2 )
+		return 0;
+
+	uint64_t sum{ 0 };
+
+	std::ifstream ifs( argv[ 1 ] );
+	for( std::string s; std::getline( ifs, s ); )
+	{
+		char * e{ nullptr };
+		uint64_t const val{ std::strtoull( s.data(), &e, 10 ) };
+
+		std::vector< uint64_t > v;
+
+		std::istringstream iss( e + 2 );
+		while( !iss.eof() )
+		{
+			uint64_t x{ 0 };
+			iss >> x;
+
+			if( v.empty() )
+			{
+				v.push_back( x );
+
+				continue;
+			}
+
+			std::vector< uint64_t > vv;
+			for( uint64_t a : v )
+			{
+				vv.emplace_back( a + x );
+				vv.emplace_back( a * x );
+
+				uint64_t p{ x };
+				while( p )
+				{
+					a *= 10;
+					p /= 10;
+				}
+				vv.emplace_back( a + x );
+			}
+
+			v = std::move( vv );
+		}
+
+		if( std::find( v.cbegin(), v.cend(), val ) != v.cend() )
+			sum += val;
+	}
+
+	std::cout << "result = " << sum << std::endl;
+
+	return 0;
+}
